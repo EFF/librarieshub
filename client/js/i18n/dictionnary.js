4b92e8e38f33d5b4489c3fa6b55b5fdef1034cca
jack_dbernier <jdesjardinsbernier@gmail.com>
2011-11-19 03:15:31 -0500

    déplacer certains fichiers js dans libs + remote des fichier inutiles json

--- client/js/i18n/dictionnary.js
+++ client/js/i18n/dictionnary.js
@@ -8,6 +8,7 @@ var dictionnary = {
     'Available at &1 library' : 'Disponible à la bibliothèque &1',
     'Available on Google Book' : 'Disponible sur Google Books',
     'Best price on Amazon : $&1' : 'Meilleur prix sur Amazon: &1$',
-    'Best price on Amazon : N/A' : 'Meilleur prix sur Amazon: N/D'
+    'Best price on Amazon : N/A' : 'Meilleur prix sur Amazon: N/D',
+    'about us!' : 'à propos!'
   }
 }
