ca3e0a9cf8499cd9d5191223d8573e7dab1ae0a4
jack_dbernier <jdesjardinsbernier@gmail.com>
2011-11-22 00:12:31 -0500

    correction d'une faute de francais

--- locales/fr.js
+++ locales/fr.js
@@ -1,6 +1,6 @@
 {
 	"index_title": "Trouver un livre",
-	"about": "Utilisez nous pour trouver un livre dans une des bibliothèques ouvertes du Québec ou chez sur Amazon",
+	"about": "Utilisez nous pour trouver un livre dans une des bibliothèques ouvertes du Québec ou en ligne sur Amazon",
 	"loading": "Chargement...",
 	"title": "Libraries Hub - La où les livres se rencontrent",
 	"Hello": "Bonjour",
