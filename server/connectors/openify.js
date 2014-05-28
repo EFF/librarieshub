02a4087d996c98e01a3a75ed3c698227793d426b
Olivier Sylvain <g.oliviersylvain@gmail.com>
2014-05-28 18:14:52 -0400

    Refactoring of ui

--- server/connectors/openify.js
+++ server/connectors/openify.js
@@ -3,7 +3,7 @@ var request = require('request');
 module.exports = {
     search: function(query, callback){
         var options = {
-            url: 'http://localhost:3000/quebec-city/libraries-catalog',
+            url: 'http://localhost:5000/v0/quebec-city/libraries-catalog',
             qs: query,
             json: true
         };
